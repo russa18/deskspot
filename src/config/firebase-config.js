2b9df6f82198eef141075abc0bb3b224d56e2df0
russa18 <rushikesh.sawant.1690@gmail.com>
2025-01-16 16:00:49 +0530

    module error fixes

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -19,6 +19,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
+
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
