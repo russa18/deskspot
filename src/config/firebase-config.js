b8d5df9cff8d363661f50dde4dae0918b6b93265
Rushikesh Sawant <64061109+russa18@users.noreply.github.com>
2025-01-16 16:15:32 +0530

    Update firebase-config.js
    api key

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -10,7 +10,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: "AIzaSyBEnSCwOI3IrmxV5gjc9wbpgL1giKVtI1I",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
